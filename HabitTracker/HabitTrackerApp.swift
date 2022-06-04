88953b9644a00f257ae50d3e133535f045337096
ygeras <86482316+Yus312@users.noreply.github.com>
2022-06-04 13:14:10 +0600

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Yuri Gerasimchuk on 04.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
